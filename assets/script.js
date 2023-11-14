0c712cde59dcbae082cd7099fdda1be2cc63bfae
SalxIeva <ieva.saldukaite@gmail.com>
2023-11-14 12:07:38 +0000

    timeblocks removed from html and added with javascript

--- assets/script.js
+++ assets/script.js
@@ -2,38 +2,50 @@
 console.log("peanut");
 
 $(document).ready(function() {
-
-var startHour = "8am";
-var endHour = "5pm";
 // Display the current day at the top of the calender when a user opens the planner.
 var currentDay = $("#currentDay");
 var today = dayjs();
 $("#currentDay").text(today.format("dddd, MMMM D[th]"));
-// var currentTime = ();
-function timeNow() {
-    var currentTime = dayjs().format("HH:mm A");
-console.log(currentTime);
-}
-timeNow();
 
-
-
-//     // Loop through the time intervals and generate time blocks
-//     for (let i = 0; i <= end.diff(start, 'minute') / interval; i++) {
-//       var currentTime = start.add(i * interval, 'minute');
-//       // Append the row to the timeblocks container
-//       timeblockContainer.append(row);
-//     }
+// Present timeblocks for standard business hours when the user scrolls down.
+    // start and end hours
+    var startHour = 8;
+    var endHour = 17;
   
-//     // check to indicate that the function has been called
-//     console.log("Timeblocks generated successfully");
-//   }
+    // Function to generate time blocks added including startTime, endTime and time Interval to generate timeblocks every 1 hour
+    function generateTimeblocks(startTime, endTime, interval) {
+        // var timeblockContainer created to represent #timeblocks
+      var timeblockContainer = $("#timeblocks");
   
-//   // Call the function with start time, end time, and interval
-//   generateTimeblocks("08:00", "17:00", 60);
+      // Loop through the time intervals and generate time blocks
+      for (let i = startTime; i <= endTime; i++) {
+        // currentTime added for tracking planner times using dayjs
+        var currentTime = dayjs().hour(i).minute(0).second(0);
+        // <div> with class row added using bootstrap grid
+        var timeblockHTML = $("<div>").addClass("row");
   
-// Call the function with your desired start time, end time, and interval
-
+        // Time blocks added to the row
+        $("<div>")
+          .addClass("col-md-1 hour time-block")
+          .text(currentTime.format('HH:mm'))
+          .appendTo(timeblockHTML);
+  
+        // user-input column created to the same row
+        $("<div>").addClass("col-md-10 user-input").appendTo(timeblockHTML);
+  
+        // submit-btn column added to the same row
+        $("<div>").addClass("col-md-1 submit-btn").appendTo(timeblockHTML);
+  
+        // Append the row to the timeblocks container
+        timeblockContainer.append(timeblockHTML);
+      }
+  
+      // checking to indicate that the function has been called
+      console.log("Timeblocks generated successfully");
+    }
+  
+    // Call the function initially
+    generateTimeblocks(startHour, endHour, 1);
 // Color-code each timeblock based on past, present, and future when the timeblock is viewed.
 
 // Allow a user to enter an event when they click a timeblock
